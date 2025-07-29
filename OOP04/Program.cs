8d35119b90d09c0a15b7174c6f39080f1b53eb06
SaraMohamed-20190234 <saramohamed12656@gmail.com>
2025-07-29 19:31:49 +0300

    PolyMorphism

--- OOP04/Program.cs
+++ OOP04/Program.cs
@@ -2,9 +2,30 @@
 {
     internal class Program
     {
+        #region MethodOverloading
+        public static int Sum(int X , int Y)
+        {
+            return X + Y;
+        }
+        public static double Sum(double X, double Y)
+        {
+            return X + Y;
+        }
+        public static double Sum(double X, double Y, double Z)
+        {
+            return X + Y + Z;
+        }
+
+        #endregion
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region polymerphism
+            #region overloading
+            Console.WriteLine(Sum(10, 20)); // Calls the int version
+            Console.WriteLine(Sum(10.5, 20.5)); // Calls the double version
+            Console.WriteLine(Sum(10.5, 20.5, 30.5)); // Calls the double version with three parameters
+            #endregion
+            #endregion
         }
     }
 }
