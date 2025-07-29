7c36ff539a00a97bc00dd8b01032b189f5aee913
SaraMohamed-20190234 <saramohamed12656@gmail.com>
2025-07-29 22:12:50 +0300

    Q3 - Complex Number

---
+++ assignment04/ComplexNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment04
+{
+    internal class ComplexNumber
+    {
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
+
+        public ComplexNumber(double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+
+        // Overload + operator
+        public static ComplexNumber operator +(ComplexNumber c1, ComplexNumber c2)
+        {
+            return new ComplexNumber(c1.Real + c2.Real, c1.Imaginary + c2.Imaginary);
+        }
+
+        // Overload - operator
+        public static ComplexNumber operator -(ComplexNumber c1, ComplexNumber c2)
+        {
+            return new ComplexNumber(c1.Real - c2.Real, c1.Imaginary - c2.Imaginary);
+        }
+        public override string ToString()
+        {
+            return $"{Real} + {Imaginary}i";
+        }
+    }
+}
--- assignment04/Program.cs
+++ assignment04/Program.cs
@@ -11,15 +11,28 @@
             //Console.WriteLine($"Add two doubles: 5.5 + 3.2 = {calc.Add(5.5, 3.2)}");
             #endregion
             #region Q2 - Rectangle
-            Rectangle rect1 = new Rectangle();
-            Rectangle rect2 = new Rectangle(5, 10);
-            Rectangle rect3 = new Rectangle(7);
-            Console.WriteLine(rect1);
-            Console.WriteLine(rect2);
-            Console.WriteLine(rect3);
+            //Rectangle rect1 = new Rectangle();
+            //Rectangle rect2 = new Rectangle(5, 10);
+            //Rectangle rect3 = new Rectangle(7);
+            //Console.WriteLine(rect1);
+            //Console.WriteLine(rect2);
+            //Console.WriteLine(rect3);
             #endregion
 
+            #region Q3 - Complex Number
+            //ComplexNumber num1 = new ComplexNumber(3, 2);
+            //ComplexNumber num2 = new ComplexNumber(1, 4);
 
+            //ComplexNumber sum = num1 + num2;
+            //Console.WriteLine(num1);
+            //Console.WriteLine(num2);
+            //Console.WriteLine("-----sum--------");
+            //Console.WriteLine(sum);
+            //ComplexNumber difference = num1 - num2;
+            //Console.WriteLine("-----difference--------");
+            //Console.WriteLine(difference);
+
+            #endregion
         }
     }
 }
