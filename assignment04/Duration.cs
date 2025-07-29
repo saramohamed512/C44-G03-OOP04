340ef40203c9887d246d4e3d5e0654890fda5e25
SaraMohamed-20190234 <saramohamed12656@gmail.com>
2025-07-29 22:30:26 +0300

    part2 q1

---
+++ assignment04/Duration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment04
+{
+    internal class Duration
+    {
+        #region Attributes 
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+        #endregion
+    }
+}
