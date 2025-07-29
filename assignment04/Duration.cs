9868062dda5489956bdac8b69c88da1a182c96f8
SaraMohamed-20190234 <saramohamed12656@gmail.com>
2025-07-29 22:33:39 +0300

    part2 q1 overrides

--- assignment04/Duration.cs
+++ assignment04/Duration.cs
@@ -13,5 +13,35 @@ namespace assignment04
         public int Minutes { get; set; }
         public int Seconds { get; set; }
         #endregion
+
+        #region overrides 
+
+        public override string ToString()
+        {
+            string result = "";
+            if (Hours > 0)
+                result += $"Hours: {Hours}, ";
+            if (Minutes > 0 || Hours > 0)
+                result += $"Minutes: {Minutes}, ";
+            result += $"Seconds: {Seconds}";
+
+            return result;
+        }
+
+    
+        public override bool Equals(object obj)
+        {
+            if (obj == null || GetType() != obj.GetType())
+                return false;
+
+            Duration other = (Duration)obj;
+            return Hours == other.Hours && Minutes == other.Minutes && Seconds == other.Seconds;
+        }
+
+        public override int GetHashCode()
+        {
+            return Hours.GetHashCode() ^ Minutes.GetHashCode() ^ Seconds.GetHashCode();
+        }
+        #endregion
     }
 }
